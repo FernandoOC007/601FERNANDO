af00ca40141c0a3ab29529103b4f74c832c370ba
Fernando Orozco <ferchooc07@gmail.com>
2023-05-19 11:24:10 -0600

    Se agrego V5TRIFIV

---
+++ 5 V4TRIFIV/src/pkg5/v4trifiv/V4TRIFIV.java
@@ -0,0 +1,25 @@
+package pkg5.v4trifiv;
+
+
+import FUNCIONES.funcion_multiplo;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class V4TRIFIV {
+
+    public static void main(String[] args) throws IOException {
+        //DECLARA VARIABLES
+        BufferedReader bufEntrada=new BufferedReader(new InputStreamReader(System.in));
+        int numero;
+        //OBJETO 
+        funcion_multiplo mult = new funcion_multiplo();
+        //ENTRADA DE DATOS
+        System.out.println("Dame numero");
+          numero = Integer.parseInt(bufEntrada.readLine() );
+          //SALIDA DE DATOS 
+          System.out.println(mult.retornaMultiplo(numero));
+        
+    }
+    
+}
