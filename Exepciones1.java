8948b5d66fd7f72b6e4e19112dd4f7b60167af54
FernandoOC007 <134037779+FernandoOC007@users.noreply.github.com>
2023-07-07 09:40:24 -0600

    Add files via upload
    se agregan Excepciones

---
+++ Exepciones1.java
@@ -0,0 +1,25 @@
+
+
+package exepciones1;
+
+import javax.swing.JOptionPane;
+
+
+public class Exepciones1 {
+
+  
+    
+   public static void main(String[] args) {
+        
+        int x,y;
+        
+        try {
+        x = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese numero 1:", "Solicito datos", 3));
+    }catch (NumberFormatException n){
+        
+        System.out.println (n.getMessage() + " NO ES UN NUMERO ENTERO");
+    }
+        
+    }
+    
+}
