dfa737396533d6c4e814626ff29dbfec6afdd600
FernandoOC007 <134037779+FernandoOC007@users.noreply.github.com>
2023-08-02 00:24:10 -0600

    Delete ConversacionDAO.java

--- ConversacionDAO.java
+++
@@ -1,9 +0,0 @@
-package com.app.sockets.DAO;
-
-public interface ConversacionDAO {
-
-    public void registrarConversacionA(String mensaje);
-
-    public void registrarConversacionB(String mensaje);
-
-}
